038e444d7f7aa05448b88458d3218e3ee651ad54
Mateusz Kornecki <50931078+mateuszkornecki@users.noreply.github.com>
2021-01-19 11:37:48 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import * as Highcharts from "highcharts";
+ 
+import HIndicatorsAll from "highcharts/indicators/indicators-all";
+import HDragPanes from "highcharts/modules/drag-panes";
+import HAnnotationsAdvanced from "highcharts/modules/annotations-advanced";
+import HPriceIndicator from "highcharts/modules/price-indicator";
+import HC_stock from 'highcharts/modules/stock';
+HC_stock(Highcharts);
+import HFullScreen from "highcharts/modules/full-screen";
+import HStockTools from "highcharts/modules/stock-tools";
 
+HIndicatorsAll(Highcharts);
+HDragPanes(Highcharts);
+HAnnotationsAdvanced(Highcharts);
+HPriceIndicator(Highcharts);
+HFullScreen(Highcharts);
+HStockTools(Highcharts);
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'stocktools';
+   Highcharts: typeof Highcharts = Highcharts;
+
+   chartOptions: Highcharts.Options = {
+     series: [
+       {
+         type: 'line',
+         pointInterval: 24 * 3600 * 1000,
+         data: [1, 2, 3, 4, 5]
+       }
+     ]
+   };
 }
